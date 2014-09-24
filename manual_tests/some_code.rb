63481282d4828f1b4cc424100269d33aef7fc124
Alexey Suhoviy <martinsilenn@gmail.com>
2014-09-24 03:05:31 +0300

    Update TODO

--- manual_tests/some_code.rb
+++ manual_tests/some_code.rb
@@ -26,39 +26,39 @@ resource "Update user profile" do
   end
 
   parameters do
-    # integer "page" do
-    #   description "page number"
-    #   required false
-    #   default 1
-    # end
+    integer "page" do
+      description "page number"
+      required false
+      default 1
+    end
 
-    # integer "per_page" do
-    #   description "items per page amount"
-    #   required false
-    #   default 25
-    # end
+    integer "per_page" do
+      description "items per page amount"
+      required false
+      default 25
+    end
 
-    # string "name" do
-    #   description "place name"
-    #   required true
-    # end
+    string "name" do
+      description "place name"
+      required true
+    end
 
-    # float "range" do
-    #   description "search range in km"
-    #   required false
-    #   example { rand(100) + rand.round(2) }
-    # end
+    float "range" do
+      description "search range in km"
+      required false
+      example { rand(100) + rand.round(2) }
+    end
 
-    # datetime "start_at" do
-    #   description "start at datetime"
-    #   required false
-    #   example { Time.now.to_s }
-    # end
+    datetime "start_at" do
+      description "start at datetime"
+      required false
+      example { Time.now.to_s }
+    end
 
-    # timestamp "seconds" do
-    #   description "seconds today"
-    #   example { Time.now.to_i }
-    # end
+    timestamp "seconds" do
+      description "seconds today"
+      example { Time.now.to_i }
+    end
 
     array "place_ids" do
       description "user's places ids"
@@ -77,46 +77,46 @@ resource "Update user profile" do
       end
     end
 
-    # document "user" do
-    #   description "user's parameters fields"
-    #   required true
-    #   content do
-    #     string "email" do
-    #       description "user's email value"
-    #     end
-    #     string "password" do
-    #       description "user's profile password"
-    #     end
-    #     string "first_name" do
-    #       description "user's first name"
-    #     end
-    #     string "last_name" do
-    #       description "user's last name"
-    #     end
-    #     string "country_locode" do
-    #       example { ["US", "UA"].sample }
-    #       description "Country location code"
-    #     end
+    document "user" do
+      description "user's parameters fields"
+      required true
+      content do
+        string "email" do
+          description "user's email value"
+        end
+        string "password" do
+          description "user's profile password"
+        end
+        string "first_name" do
+          description "user's first name"
+        end
+        string "last_name" do
+          description "user's last name"
+        end
+        string "country_locode" do
+          example { ["US", "UA"].sample }
+          description "Country location code"
+        end
 
-    #     document "stats" do
-    #       content do
-    #         timestamp "login_at" do
-    #           description "last login timestamp"
-    #           example { Time.now.to_i }
-    #         end
+        document "stats" do
+          content do
+            timestamp "login_at" do
+              description "last login timestamp"
+              example { Time.now.to_i }
+            end
 
-    #         integer "login_count" do
-    #           description "login count"
-    #           example { rand(10000) }
-    #         end
+            integer "login_count" do
+              description "login count"
+              example { rand(10000) }
+            end
 
-    #         string "rank" do
-    #           description "users rank"
-    #           example { ["Junior", "Middle", "Senior"].sample }
-    #         end
-    #       end
-    #     end
-    #   end
-    # end
+            string "rank" do
+              description "users rank"
+              example { ["Junior", "Middle", "Senior"].sample }
+            end
+          end
+        end
+      end
+    end
   end
 end
